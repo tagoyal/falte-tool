8f07e28b3e3a78d69f94b7ea97312715d64527d0
Tanya Goyal <tanyagoyal@utexas.edu>
2022-10-09 03:13:29 -0500

    removing unnecessary files

--- static/js/config.js
+++
@@ -1,9 +0,0 @@
-var error_dictionary = {
-    'selection-character': {'name': 'New Person not Introduced', 'type': 'singleton'},
-    'selection-event': {'name': 'Missing Information about Object/Event', 'type': 'singleton'},
-    'selection-scene': {'name': 'Abrupt Scene Transition', 'type': 'singleton'},
-    'selection-repitition': {'name': 'Repitition', 'type': 'paired'},
-    'selection-inconsistent': {'name': 'Inconsistent', 'type': 'paired'},
-    'selection-grammar': {'name': 'Ungrammatical', 'type': 'singleton'},
-    'selection-coref': {'name': 'Unclear Coreference', 'type': 'singleton'}
-    }
